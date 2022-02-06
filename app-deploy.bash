b3e43b645970873ff8fde5f243529805b0dfbf8f
seubpong.mon <pjame.fb@gmail.com>
2022-02-06 21:04:07 +0700

    Write key in base64

--- app-deploy.bash
+++ app-deploy.bash
@@ -19,14 +19,15 @@ PATH_SPEC=devops@${HOST}:${REMOTE_DIR}
 USER_SPEC=devops@${HOST}
 
 gcloud auth activate-service-account --key-file=${SA_KEY}
-
+echo "##### start"
 gcloud compute ssh ${USER_SPEC} --ssh-key-file=${KEY_FILE} --tunnel-through-iap \
     --quiet --zone=${ZONE} --command="mkdir -p ${REMOTE_DIR}"
+echo "##### end"
 
 # DO NOT cat any private key here
-gcloud compute scp --ssh-key-file=${KEY_FILE} --tunnel-through-iap --quiet --zone=${ZONE} app-start.bash ${PATH_SPEC}
-gcloud compute scp --ssh-key-file=${KEY_FILE} --tunnel-through-iap --quiet --zone=${ZONE} custom-${BRANCH}.cfg ${PATH_SPEC}/custom.cfg
-gcloud compute scp --ssh-key-file=${KEY_FILE} --tunnel-through-iap --quiet --zone=${ZONE} docker-compose.yaml ${PATH_SPEC}
+#gcloud compute scp --ssh-key-file=${KEY_FILE} --tunnel-through-iap --quiet --zone=${ZONE} app-start.bash ${PATH_SPEC}
+#gcloud compute scp --ssh-key-file=${KEY_FILE} --tunnel-through-iap --quiet --zone=${ZONE} custom-${BRANCH}.cfg ${PATH_SPEC}/custom.cfg
+#gcloud compute scp --ssh-key-file=${KEY_FILE} --tunnel-through-iap --quiet --zone=${ZONE} docker-compose.yaml ${PATH_SPEC}
 
-gcloud compute ssh ${USER_SPEC} --ssh-key-file=${KEY_FILE} --tunnel-through-iap \
-    --quiet --zone=${ZONE} --command="cd ${REMOTE_DIR}; ./app-start.bash"
+#gcloud compute ssh ${USER_SPEC} --ssh-key-file=${KEY_FILE} --tunnel-through-iap \
+#    --quiet --zone=${ZONE} --command="cd ${REMOTE_DIR}; ./app-start.bash"
