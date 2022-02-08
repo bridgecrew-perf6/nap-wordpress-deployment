f003f89d8f60b1a1fd8f46497d271c48dc527ea6
seubpong.mon <pjame.fb@gmail.com>
2022-02-08 07:32:42 +0700

    Fixed new line issue

--- app-start.bash
+++ app-start.bash
@@ -12,8 +12,11 @@ source "custom.cfg" #This file is copied and renamed by github action job
 gcloud secrets versions access latest --secret="nap-wordpress-${STAGE}-secrets" > secrets.cfg
 
 cat custom.cfg > ${ENV_FILE}
+echo "" >> ${ENV_FILE}
+
 cat secrets.cfg >> ${ENV_FILE}
 echo "" >> ${ENV_FILE}
+
 echo "DATA_DIR=${HOME}/wordpress/data" >> ${ENV_FILE} 
 
 sudo docker-compose up -d
